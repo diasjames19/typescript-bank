873ca4e991e8acc54cae04e22e61b4c4ce22aba3
diasjames19 <diasjames@hotmail.com>
2025-11-26 16:09:25 -0300

    Ajuste na classe People

--- src/models/models-peopleAccount/PeopleAccount.ts
+++ src/models/models-peopleAccount/PeopleAccount.ts
@@ -2,6 +2,7 @@ import { DioAccounts } from "../DioAccounts";
 
 export class PeopleAccount extends DioAccounts {
     doc_id: number;
+    
     constructor(name:string, accountNumber:number, doc_id:number){
         super(name,accountNumber)
         this.doc_id = doc_id
